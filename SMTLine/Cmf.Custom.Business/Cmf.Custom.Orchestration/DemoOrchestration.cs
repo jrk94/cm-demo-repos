4dc3e6d81575cb53c59ee4b6febb9a880d6c1e27
Roque <32566150+jrk94@users.noreply.github.com>
2025-06-06 12:01:29 +0100

    feat: running smt line (#2)

---
+++ SMTLine/Cmf.Custom.Business/Cmf.Custom.Orchestration/DemoOrchestration.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Cmf.Custom.Demo.Orchestration
+{
+    public class DemoOrchestration : IDemoOrchestration
+    {
+        private const string OBJECT_TYPE_NAME = "Cmf.Custom.Demo.Orchestration.DemoManagementOrchestration";
+    }
+}
---
+++ SMTLine/Cmf.Custom.IoT/Cmf.Custom.IoT.Packages/src/custom-dataplatform-tasks/README.md
@@ -0,0 +1,3 @@
+# Connect IoT - Custom Data Platform Tasks/Converters Library Package
+
+This package will be used to create custom parsers to transform machine messages to IoT Event Post Telemetry.
